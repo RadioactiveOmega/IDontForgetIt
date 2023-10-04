bdfa9cac6e5cee8790a5990e53079cf24fd9f32d
RadioactiveOmega <Argameing@ya.ru>
2023-10-04 23:33:34 +0300

    Start

---
+++ src/main/java/com/myProjects/IDontForgetIt/IDontForgetItApplication.java
@@ -0,0 +1,13 @@
+package com.myProjects.IDontForgetIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDontForgetItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDontForgetItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myProjects/IDontForgetIt/IDontForgetItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProjects.IDontForgetIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDontForgetItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
